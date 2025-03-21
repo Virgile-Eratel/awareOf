83ba851d0c93a166a4265122557c140545d8a2a0
Virgile Marty <eratel@MacBookPro.local>
2025-03-21 09:27:59 +0100

    init react tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className={"bg-red-800 h-52 w-52"}>
+          <p className={"text-black"}>bonjour</p>
+      </div>
+    </>
+  )
+}
+
+export default App
